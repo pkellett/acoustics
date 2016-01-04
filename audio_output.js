9b6ea0fc5ec509b5706132b6e56781fa7a6647fa
Paul Kellett <paul.kellett@mda-vst.com>
2016-01-04 16:42:12 +0100

    Update audio_output.js
    removed obsolete firefox version

--- audio_output.js
+++ audio_output.js
@@ -23,75 +23,26 @@ function AudioOutput(audioOutputStart, audioOutputRender)
   {
     sampleRate = audioContext.sampleRate;
     audioOutputStart(sampleRate);
-    var node = audioContext.createScriptProcessor(4096, 1, 1);
+    var node = audioContext.createScriptProcessor(2048, 1, 1);
     node.onaudioprocess = function(event) { audioOutputRender(event.outputBuffer.getChannelData(0)); } //specify callback
     node.connect(audioContext.destination);
     
     // check if we run on iOS and need a dummy oscillator
     if(navigator.userAgent.match(/(iPad|iPhone|iPod)/g))
     {
-	    var osc = context.createOscillator();
-	    osc.type = 0;
-	    osc.connect(node);
-	    osc.noteOn(0);
+      var osc = context.createOscillator();
+      osc.type = 0;
+      osc.connect(node);
+      osc.noteOn(0);
     }
   }
-  else
+  else //no audio output, but run the callback anyway
   {
-    if(typeof Audio == "function") //try Audio Data API
-    {
-      audioContext = new Audio();
-    }
-    if(audioContext && typeof audioContext.mozSetup == "function")
-    {
-      audioContext.mozSetup(1, sampleRate);
-      audioOutputStart(sampleRate);
+    document.write("<font style='background-color:yellow;'>Your browser does not support real-time audio output. Try Chrome or Firefox.</font>");
 
-      var prebufferSize = sampleRate / 2;
-      var tailPosition, tail = null;
-      var writePosition = 0;
-
-      setInterval(function()
-      {
-        var written;
-        if(tail) //Check if some data was not written in previous attempts.
-        {
-          written = audioContext.mozWriteAudio(tail.subarray(tailPosition));
-          writePosition += written;
-          tailPosition += written;
-          if(tailPosition < tail.length) //Not all the data was written, save the tail...
-            return; //... and exit the function.
-          tail = null;
-        }
-
-        //Check if we need add some data to the audio output.
-        var available = audioContext.mozCurrentSampleOffset() + prebufferSize - writePosition;
-        if(available & 1) available--; //ensure whole number of frames, if stereo
-        if(available > 0) 
-        {
-          //Render some audio data
-          var buffer = new Float32Array(available);
-          audioOutputRender(buffer);
-
-          //Write the data
-          written = audioContext.mozWriteAudio(buffer);
-          if(written < buffer.length) //Not all the data was written, save the tail
-          {
-            tail = buffer;
-            tailPosition = written;
-          }
-          writePosition += written;
-        }
-      }, 50);
-    }
-    else //no audio output, but run the callback anyway
-    {
-      document.write("<font style='background-color:yellow;'>Your browser does not support real-time audio output. Try Chrome or Firefox.</font>");
-
-      audioOutputStart(sampleRate);
-      var buffer = new Array(sampleRate / 10);
-      setInterval(function() { renderCallback(buffer); }, 100);
-    }
+    audioOutputStart(sampleRate);
+    var buffer = new Array(sampleRate / 10);
+    setInterval(function() { renderCallback(buffer); }, 100);
   }
 }
 
