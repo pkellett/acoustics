47e18ea21abd48ef2803bb8de74542b91dbb2424
Paul Kellett <paul.kellett@mda-vst.com>
2016-01-08 14:06:16 +0100

    Update audio_output.js

--- audio_output.js
+++ audio_output.js
@@ -8,10 +8,15 @@ function AudioOutput(audioOutputStart, audioOutputRender)
 {
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   var audioContext = new AudioContext();
-
+  if(!audioContext)
+  {
+    document.write("<font style='background-color:yellow;'>Your browser does not support real-time audio output. Try Chrome or Firefox.</font>");
+  }
   audioOutputStart(audioContext.sampleRate);
+
   var scriptProcessor = audioContext.createScriptProcessor(4096, 1, 1);
   scriptProcessor.onaudioprocess = function(event) { audioOutputRender(event.outputBuffer.getChannelData(0)); }
+  scriptProcessor.connect(audioContext.destination);
   
   var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
   if(iOS) //need a dummy oscillator?
@@ -19,16 +24,6 @@ function AudioOutput(audioOutputStart, audioOutputRender)
     var dummyInput = audioContext.createOscillator()
     dummyInput.type = 0;
     dummyInput.connect(scriptProcessor);
-  }
-
-  scriptProcessor.connect(audioContext.destination);
-  if(iOS)
-  {
     dummyInput.noteOn(0);
   }
-
-  //document.write("<font style='background-color:yellow;'>Your browser does not support real-time audio output. Try Chrome or Firefox.</font>");
-  //audioOutputStart(sampleRate);
-  //var buffer = new Array(sampleRate / 10);
-  //setInterval(function() { renderCallback(buffer); }, 100);
 }
